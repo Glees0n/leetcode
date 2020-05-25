ff659dea67d7c016c81fe60ba4b2d2f3dd942391
Glees0n <64979379+Glees0n@users.noreply.github.com>
2020-05-25 19:01:55 +0930

    Update first-bad-version.cpp

--- may-codeleet-challenge/first-bad-version/first-bad-version.cpp
+++ may-codeleet-challenge/first-bad-version/first-bad-version.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+bool isBadVersion(int version)
+{
+	// isBadVersion function not provided
+	return true;
+}
+
+int firstBadVersion(int n) 
+{
+	int left = 1;
+	int right = n;
+
+	while (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		if (isBadVersion(mid))
+		{
+			right = mid;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	
+	return left;
+}
+
 int main()
 {
 
